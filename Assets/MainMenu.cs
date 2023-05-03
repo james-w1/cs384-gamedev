6f90df6976c31e5ca64834706d11556d1e0816b5
JW <73392543+james-w1@users.noreply.github.com>
2023-05-03 15:53:47 +0100

    navi commit

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
 {
@@ -19,6 +20,12 @@ public class MainMenu : MonoBehaviour
     {
 	    settingsOn = !settingsOn;
 	    panel.SetActive(settingsOn);
+    }
+
+    [SerializeField] AudioMixer mixer;
+    public void SetLevel (float sliderValue)
+    {
+        mixer.SetFloat("MainVol", Mathf.Log10(sliderValue) * 20);
     }
 
     public void PlayGame() 
